1455b6d15e254cf09b7481e8f63fa0dbb17a4a57
mfuglum <mfuglum@outlook.com>
2024-03-19 18:52:32 +0100

    Change configuration architecture Move all configuration parameters to .env file Create config generation script and container Create configuration templates for all Home Assistant configuration files The config_generator script will write the HA config files based on .env and templates Change sensor ID in AppDaemon config to verify functionality after reorganization

---
+++ server/src/config_generator/config_generator.py
@@ -0,0 +1,39 @@
+import os
+from jinja2 import Template
+
+def parse_environment_file(env_file):
+    env = {}
+    with open(env_file, 'r') as file:
+        env_file = file.read()
+        for pair in env_file.split("\n"):
+            if "=" in pair:
+                key, value = pair.split("=", 1)
+                env[key.strip()] = value.strip()
+    print(env)
+    return env
+
+def generate_config(template_path, config_path, env_file):
+    # Read the template file and generate the config file
+    with open(template_path, 'r') as file:
+        file_contents = file.read()
+        template = Template(file_contents)
+        env = parse_environment_file(env_file)
+        rendered_template = template.render(env)
+    
+        # Write the rendered template to the actual configuration file
+        with open(config_path, 'w') as file:
+            file.write(rendered_template)
+
+def main():
+    template_and_config_pairs = {
+        "./automations_template.yaml": "./server/homeassistant/config/automations.yaml",
+        "./configuration_template.yaml": "./server/homeassistant/config/configuration.yaml",
+        "./influxdb_template.yaml": "./server/homeassistant/config/influxdb.yaml",
+    }
+
+    env_file = ".env"
+    for template_path, config_path in template_and_config_pairs.items():
+        generate_config(template_path, config_path, env_file)
+
+if __name__ == "__main__":
+    main()
