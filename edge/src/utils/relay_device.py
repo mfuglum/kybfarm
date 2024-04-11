302da77078f540bf8571bbca9129ca7075d476d0
mfuglum <mfuglum@outlook.com>
2024-04-11 14:20:02 +0200

    Add relay activation class and test functionality in main

---
+++ edge/src/utils/relay_device.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import threading
+
+# Relay device class to control individual relays on TTL-RELAY02 / 04 / 08 control boards
+class relay_device:
+    def __init__ (self, pin):
+        # Disable warnings
+        GPIO.setwarnings(False)
+        # Set the pin numbering mode
+        GPIO.setmode(GPIO.BOARD)
+        # Set the pin as output
+        GPIO.setup(pin, GPIO.OUT)
+        # Set the pin to high as relay is OFF in high state
+        GPIO.output(pin, GPIO.HIGH)
+        self.pin = pin
+
+    # Turn the relay ON
+    def turn_on(self):
+        GPIO.output(self.pin, GPIO.LOW)
+        
+    # Turn the relay OFF
+    def turn_off(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    # Turn the relay ON for a specified amount of time (seconds)
+    def turn_on_for(self, time):
+        self.turn_on()
+        # Create a thread to turn off the relay after the specified time (seconds)
+        threading.Timer(time, self.turn_off).start()
+    
+    # Cleanup the GPIO i.e. set all used pins in program back to input mode
+    def cleanup(self):
+        GPIO.cleanup()
