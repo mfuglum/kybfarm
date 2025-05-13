ebd6466f3a002226464983f9d9441b3ec93669c8
kaspermads <kaspergm@hotmail.no>
2025-05-13 10:47:23 +0200

    Attempt to fix ref co2

--- edge/src/actuator_instances/CO2_control.py
+++ edge/src/actuator_instances/CO2_control.py
@@ -14,7 +14,7 @@ CO2_pid = PController(Kp=0.1)
 def on_message_REFCO2_CMD_REQ(client, userdata, msg):
     cmd_msg = json.loads(msg.payload)
     try:
-        print("Desired CO2 level", cmd_msg, "\n")
+        print("Desired CO2 level", cmd_msg["value"], "\n")
         if cmd_msg["cmd"] == "adjust_ref_co2":
             latest_CO2_data["REF_CO2"]= float((cmd_msg["value"]))
             ref_CO2 = latest_CO2_data["REF_CO2"]
